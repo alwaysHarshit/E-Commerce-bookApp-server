f5d6682d0a53bcf8f3a6a4ac332bdee88d677c07
Harshit <studyharshit21@gmail.com>
2026-04-26 04:57:23 +0530

    enable service discovery with Eureka client; update dependencies in `pom.xml` to include Spring Cloud modules
    Took 26 minutes
    
    Took 53 minutes

--- Auth-Service/src/main/java/org/booknest/auth/AuthServiceApplication.java
+++ Auth-Service/src/main/java/org/booknest/auth/AuthServiceApplication.java
@@ -2,11 +2,12 @@ package org.booknest.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
 @EnableAsync
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
