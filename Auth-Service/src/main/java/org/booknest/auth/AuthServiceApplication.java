4c471702e4bf4635bb574b8c3f06df60fc6974b5
Harshit <studyharshit21@gmail.com>
2026-04-26 23:39:02 +0530

    add request logging filter, enable async processing, comment out context path, and update dependencies
    Took 35 minutes

--- Auth-Service/src/main/java/org/booknest/auth/AuthServiceApplication.java
+++ Auth-Service/src/main/java/org/booknest/auth/AuthServiceApplication.java
@@ -2,7 +2,8 @@ package org.booknest.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
---
+++ Auth-Service/src/main/java/org/booknest/auth/config/RequestLoggingFilter.java
@@ -0,0 +1,26 @@
+package org.booknest.auth.config;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        System.out.println("👉 AUTH SERVICE HIT: " + request.getRequestURI());
+
+        filterChain.doFilter(request, response);
+    }
+}
