8f8ab83706e42e3849b4a370df5aa93c1115252d
Harshit <studyharshit21@gmail.com>
2026-05-13 11:02:05 +0530

    Move CORS configuration from application.yaml to CorsConfig.java for better manageability v2.0
    Took 52 minutes

---
+++ api-gateway/src/main/java/org/booknest/apigateway/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.booknest.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
+    }
+}
