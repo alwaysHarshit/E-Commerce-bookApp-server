3c12e07fb66c4635fb5989891d0dffe21ea37c9f
Harshit <studyharshit21@gmail.com>
2026-05-18 00:09:44 +0530

    feat: enable pageable support in PaymentRepository
    Took 8 minutes

--- payment-service/src/main/java/org/booknest/paymentservice/repo/PaymentRepository.java
+++ payment-service/src/main/java/org/booknest/paymentservice/repo/PaymentRepository.java
@@ -1,10 +1,11 @@
 package org.booknest.paymentservice.repo;
 
 import org.booknest.paymentservice.entity.Payment;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
